09522d270947892058b879473ff5472c54e91af5
Anoop Patel <anooppatel582@gmail.com>
2026-01-18 16:17:20 +0530

    Making neccessary to deploy it.

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,14 @@ require("dotenv").config();
 const videoRoutes = require("./routes/video");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/video", videoRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
